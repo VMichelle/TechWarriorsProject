babcf63407198b679f0c623afec78266cf3fb279
ninja <ebecker03@msn.com>
2019-06-05 16:53:21 -0500

    added email validation with regex

--- TechWarriors/src/app/registration/registration.component.ts
+++ TechWarriors/src/app/registration/registration.component.ts
@@ -42,18 +42,4 @@ export class RegistrationComponent implements OnInit {
 
 }
 
-// import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-registration',
-//   templateUrl: './registration.component.html',
-//   styleUrls: ['./registration.component.scss']
-// })
-// export class RegistrationComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit() {
-//   }
-
-// }
