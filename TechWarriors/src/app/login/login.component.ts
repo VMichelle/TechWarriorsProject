1af40abf95e1d01f5d1ce6a5966378b2e01c9a06
Michelle Vu <th.michellevu@gmail.com>
2019-06-06 15:48:36 -0500

    trying out firebase authentication

--- TechWarriors/src/app/login/login.component.ts
+++ TechWarriors/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth'
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email: string;
+  password: string;
+
+  firebase = require('firebase');
+  firebaseui = require('firebaseui');
+
+  ui = new firebaseui.auth.AuthUI(firebase.auth());
+
   constructor() { }
 
   ngOnInit() {
+    ui.start('#firebaseui-auth-container', {
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      // Other config options...
+    });
+
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
+
   }
+
+  
+
+  // checkLogin(){
+  // firebase.auth().signInWithEmailAndPassword(this.email, this.password).catch(function(error) {
+  //   // Handle Errors here.
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+  //   // ...
+  // });
+  // }
 
 }
