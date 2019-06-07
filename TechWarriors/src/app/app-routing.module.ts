f442c3e6c2f62061c5e3f7b8d7250a6b96c2def1
Michelle Vu <th.michellevu@gmail.com>
2019-06-07 13:57:18 -0500

    login works

--- TechWarriors/src/app/app-routing.module.ts
+++ TechWarriors/src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
 import { RegistrationComponent } from './registration/registration.component';
 
+
 const routes: Routes = [
   { path: '', redirectTo: '/homepage', pathMatch: 'full'},
   { path: 'login', component: LoginComponent },
-  // { path: 'login', component: LoginComponent, canActivate: [AuthGenericService] },
   { path: 'homepage', component: HomepageComponent },
   { path: 'about', component: AboutComponent },
   { path: 'registration', component: RegistrationComponent },
