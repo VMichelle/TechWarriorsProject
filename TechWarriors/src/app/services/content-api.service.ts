a58268be6889e0dec0967c30cad1937f2d5d9e59
AmySand <amy.sand@midlandu.edu>
2019-06-07 10:06:40 -0500

    will serve

--- TechWarriors/src/app/services/content-api.service.ts
+++ TechWarriors/src/app/services/content-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,54 +8,4 @@ export class ContentAPIService {
   constructor() { }
 
 
-  addContent() {function(data) {
-    var collection = firebase.firestore().collection('content');
-    return collection.add(data);
-  };}
-
- Content.prototype.getAllContent = function(renderer) {
-    var query = firebase.firestore()
-        .collection('content')
-        .orderBy('title', 'desc')
-        .limit(50);
-  
-    this.getDocumentsInQuery(query, renderer);
-  };
-
-  Content.prototype.getDocumentsInQuery = function(query, renderer) {
-    query.onSnapshot(function(snapshot) {
-      if (!snapshot.size) return renderer.empty(); // Display "There is no content".
-  
-      snapshot.docChanges().forEach(function(change) {
-        if (change.type === 'removed') {
-          renderer.remove(change.doc);
-        } else {
-          renderer.display(change.doc);
-        }
-      });
-    });
-  };
-Content.prototype.getFilteredContent = function(filters, renderer) {
-    var query = firebase.firestore().collection('content');
-  
-    if (filters.category !== 'Any') {
-      query = query.where('type', '==', filters.type);
-    }
-  
-    if (filters.city !== 'Any') {
-      query = query.where('city', '==', filters.city);
-    }
-  
-    if (filters.price !== 'Any') {
-      query = query.where('price', '==', filters.price.length);
-    }
-  
-    if (filters.sort === 'Title') {
-      query = query.orderBy('title', 'desc');
-    } else if (filters.sort === 'Type') {
-      query = query.orderBy('type', 'desc');
-    }
-  
-    this.getDocumentsInQuery(query, renderer);
-  };
 }
