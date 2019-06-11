80929191c1a5b2621b4b7d6024706ab2394245fe
Errorcod3 <mantus@live.com>
2019-06-11 16:38:14 -0500

    setup for host build

--- TechWarriors/src/environments/environment.prod.ts
+++ TechWarriors/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCx4_9HwkIv3feF77HXqjUyRGTfrA4AlA0",
+    authDomain: "techwarriors-1c02e.firebaseapp.com",
+    databaseURL: "https://techwarriors-1c02e.firebaseio.com",
+    projectId: "techwarriors-1c02e",
+    storageBucket: "techwarriors-1c02e.appspot.com",
+    messagingSenderId: "975946604095",
+    appId: "1:975946604095:web:61cc9a9731f5bc91"
+  }
 };
