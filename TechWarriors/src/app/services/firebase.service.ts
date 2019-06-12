9a9d97d1b2f311d2ac3f9d4e776e57610c539a81
cviacrucis <47760402+cviacrucis@users.noreply.github.com>
2019-06-12 10:54:51 -0500

    changes

--- TechWarriors/src/app/services/firebase.service.ts
+++ TechWarriors/src/app/services/firebase.service.ts
@@ -8,41 +8,15 @@ export class FirebaseService {
 
   constructor(public db: AngularFirestore) {}
 
-//   getAvatars(){
-//       return this.db.collection('/avatar').valueChanges()
-//   }
 
   getFile(userKey){
     return this.db.collection('files').doc(userKey).snapshotChanges();
   }
-
-//   updateUser(userKey, value){
-//     value.nameToSearch = value.name.toLowerCase();
-//     return this.db.collection('users').doc(userKey).set(value);
-//   }
 
 
   getFiles(){
     return this.db.collection('files').snapshotChanges();
   }
 
-//   searchUsers(searchValue){
-//     return this.db.collection('users',ref => ref.where('nameToSearch', '>=', searchValue)
-//       .where('nameToSearch', '<=', searchValue + '\uf8ff'))
-//       .snapshotChanges()
-//   }
 
-//   searchUsersByAge(value){
-//     return this.db.collection('users',ref => ref.orderBy('age').startAt(value)).snapshotChanges();
-//   }
-
-
-  // createFile(value){
-  //   return this.db.collection('files').add({
-  //     UID: value.UID,
-  //     title: value.title,
-  //     description: value.description,
-  //     downloadUrl: "",
-  //   });
-  // }
 }
