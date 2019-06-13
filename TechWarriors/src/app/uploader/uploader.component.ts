9a1ff103e871ebe22f3db722fd1438c7423c6f4b
Grayton Savickas <graytonsavickas@mandalorian.mlc.edu>
2019-06-13 10:52:30 -0500

    Adds upload service and creates entries in files collection

--- TechWarriors/src/app/uploader/uploader.component.ts
+++ TechWarriors/src/app/uploader/uploader.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+// import { Title } from '@angular/platform-browser';
+import {  UploaderService } from '../services/uploader.service'
+
 
 @Component({
   selector: 'uploader',
@@ -7,17 +11,58 @@ import { Component } from '@angular/core';
 })
 export class UploaderComponent {
 
+  exampleForm: FormGroup;
+
   isHovering: boolean;
 
   files: File[] = [];
 
+  fileInfo: Object[] = [];
+
+  // validation_messages = {
+  //   'title': [
+  //     { type: 'required', message: 'Title is required.' }
+  //   ],
+  //   'description': [
+  //     { type: 'required', message: 'Description is required.' }
+  //   ]
+  // };
+
+  constructor(private fb: FormBuilder, public UploaderService: UploaderService) { 
+    const form = new FormGroup({
+    title: new FormControl(),
+    description: new FormControl()
+  }); }
+
+    ngOnInit() {
+      this.createForm();
+    }
+
+    createForm() {
+      this.exampleForm = this.fb.group({
+        title: [''],
+        description: ['']
+      });
+    }
+
   toggleHover(event: boolean) {
     this.isHovering = event;
   }
-
-  onClick(files: FileList) {
+  
+  onDrop(files: FileList
+    ) {
     for (let i = 0; i < files.length; i++) {
       this.files.push(files.item(i));
     }
+    console.log('onDrop happend');
+  }
+
+  addFile(file){
+    this.fileInfo.push(file)
+  }
+
+  onSubmit(){
+    this.UploaderService.createFile(this.exampleForm.value, this.fileInfo);
+    console.log('onSubmit happend');
   }
 }
