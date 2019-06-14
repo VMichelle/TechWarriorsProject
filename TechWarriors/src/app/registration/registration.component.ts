6ada5d3bd104b58e529dad97fa9f6e7e54de4bc5
Michelle Vu <th.michellevu@gmail.com>
2019-06-14 11:51:51 -0500

    update

--- TechWarriors/src/app/registration/registration.component.ts
+++ TechWarriors/src/app/registration/registration.component.ts
@@ -3,8 +3,7 @@ import { RegServiceService } from '../services/reg-service.service'
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
-// import { Observable } from 'rxjs/Observable';
-// import 'rxjs/add/operator/map';
+
 
 
 
@@ -32,7 +31,7 @@ export class RegistrationComponent implements OnInit{
   ) { }
 
   addUser(){
-    this.afs.collection('users').add({'admin': this.admin = false, 'first name': this.registerForm.value.firstName, 'last name': this.registerForm.value.lastName, 'email': this.email});
+    this.afs.collection('users').add({'first name': this.registerForm.value.firstName, 'last name': this.registerForm.value.lastName, 'email': this.email});
   }
 
    ngOnInit() {
@@ -46,9 +45,9 @@ export class RegistrationComponent implements OnInit{
 
 
 
-   tryRegister(value){
+   tryRegister(){
      this.addUser();
-     this.authService.doRegister(value)
+     this.authService.doRegister(this.registerForm.value)
      .then(res => {
        console.log(res);
        this.errorMessage = "";
@@ -59,88 +58,5 @@ export class RegistrationComponent implements OnInit{
        this.errorMessage = err.message;
      })
     }
+
 }
-
-
-
-// import { Component, OnInit } from '@angular/core';
-// import { RegServiceService } from '../services/reg-service.service';
-// import { registerInterface } from '../services/userInterface';
-// import { AngularFireAuthModule } from '@angular/fire/auth';
-// import { authInterface } from '../services/auth-interface'
-
-// @Component({
-//   selector: 'app-registration',
-//   templateUrl: './registration.component.html',
-//   styleUrls: ['./registration.component.scss']
-// })
-// export class RegistrationComponent implements OnInit {
-
-//   constructor(private regService: RegServiceService) { }
-//   submitted: boolean;
-//   showSuccessMessage: boolean;
-//   formControls = this.regService.form.controls;
-//   errorMessage: String = "";
-//   successMessage: String = "";
-
-//   userResponse: registerInterface ={
-//     firstName: "",
-//     lastName: "",
-//     email: "",
-//     password: ""
-//   }
-
-  
-
-
-//   ngOnInit() {
-//   }
-
-//   onSubmit() {
-//     this.submitted = true;
-//     if (this.regService.form.valid) {
-//       if (this.regService.form.get('$key').value == null)
-//         this.regService.insertUser(this.userResponse);
-//         this.showSuccessMessage = true;
-//         setTimeout(() => this.showSuccessMessage = false, 3000);
-//       this.submitted = false;
-//       this.userResponse.firstName= "";
-//       this.userResponse.lastName= "";
-//       this.userResponse.email= "";
-//       this.userResponse.password= "";
-
-      
-//         this.regService.doRegister(authInterface)
-//         .then(res => {
-//           console.log(res);
-//           this.errorMessage = "An error occurred!  Please try again!";
-//           this.successMessage = "Your account has been created";
-//         }, err => {
-//           console.log(err);
-//           this.errorMessage = err.message;
-//           this.successMessage = "";
-//         })
-      
-//     }
-
-    
-
-//   }
-
-// }
-
-
-
-
-
-
-
-
-  //  onSubmit(){
-  //   this.submitted = true;
-  //   if (this.tryRegister.arguments.valid) {
-  //     if (this.authService.doRegister.arguments
-  //       this.regService.insertUser(this.userResponse);
-  //       this.showSuccessMessage = true;
-  //       setTimeout(() => this.showSuccessMessage = false, 3000);
-  //  }
